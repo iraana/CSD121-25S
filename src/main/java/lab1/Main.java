9df49a56f6700547d6765d7d72990e1970e8747c
tyvdvb <ira1980nazar@gmail.com>
2025-05-12 15:48:01 -0400

    Fix comments

--- src/main/java/lab1/Main.java
+++ src/main/java/lab1/Main.java
@@ -1,5 +1,10 @@
-package lab1;
+/**
+ * Iryna Nazar
+ * Lab-1
+ * 12.05.2025
+ */
 
+package lab1;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -17,10 +22,10 @@ public class Main {
      * @param courses       The list of courses the student is enrolled in
      */
     public static  void  saveStudentInfoToFile(String studentName, int studentId, List<String> courses){
-        File file = new File("studentInfo.txt");
+        File file = new File("studentInfo.txt"); // creates a file for saving student info
 
-        // writing student info to the file ( Student name, ID and enrolled courses )
-        try (FileWriter fw = new FileWriter(file,false)) {
+        // Writing student info to the file ( Student name, ID and enrolled courses )
+        try (FileWriter fw = new FileWriter(file,false)) { // 'false' for append  means overwriting the file if it already exists with some content inside
             fw.write("Student name: " + studentName + "\n");
             fw.write("Student ID: " + studentId + "\n");
             fw.write("Enrolled Courses:\n");
@@ -35,11 +40,12 @@ public class Main {
         }
     }
     public static void main(String[] args) {
-        int exit = 1;
+        int exit = 1; // a variable responsible for exit condition of the program
 
-
+        // Loop to run the program printing out student's information until the user decides to exit
         while (exit != 0) {
-            List<String> courses = new ArrayList<>(); // filled array with the courses
+            List<String> courses = new ArrayList<>(); // initialize a list for enrolled courses
+            // Adding courses the student is enrolled this term in to the list
             courses.add("Programming Concepts I");
             courses.add("Databases I");
             courses.add("Intro to Web Development");
@@ -50,29 +56,33 @@ public class Main {
                 int student_id = 24048851;
                 String student_name = "Iryna Nazar";
 
+                // Prompt the user to enter their student ID
                 System.out.print("Enter your student number: ");
                 Scanner scanner = new Scanner(System.in);
                 int student_number = scanner.nextInt();
 
-                // checks if student's id is valid
+                // Check if student's id is valid
                 if (student_number == student_id) {
                     System.out.println("Welcome, " + student_name);
                     System.out.println("Your have "+ courses.size()+" courses");
+                    //Print the list of enrolled courses for the student
                     System.out.println("Enrolled Courses:");
                     for (String course : courses) {
                         System.out.println("- " + course);
                     }
 
+                    // Call the method to save student info to the file
                     saveStudentInfoToFile(student_name, student_id, courses);
 
                 } else {
+                    // If the id is wrong, prints the error message and asks t
                     System.out.println("Not a valid student number");
                 }
-                // an option to end the program
+                // Ask the user if he wants to end the program
                 System.out.println("Do you want to exit the program? (0 to exit, 1 to continue): ");
                 exit = scanner.nextInt();
 
-            } catch (Exception e) {
+            } catch (Exception e) { // it catches any exceptions that might happen during the user input, for example if he types in 'abcdefg' for a student id prompt
                 System.out.println("Something went wrong. Please try again.");
             }
 
